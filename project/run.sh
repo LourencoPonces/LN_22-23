66cd35153f1c44ca86beca6fc086d744edae4963
LourencoPonces <lourenco.poncesduarte@gmail.com>
2022-10-10 21:45:54 +0100

    change some details

--- project/run.sh
+++ project/run.sh
@@ -4,31 +4,20 @@ mkdir -p compiled images
 
 # ############ Convert friendly and compile to openfst ############
 for i in friendly/*.txt; do
-	#echo "Converting friendly: $i"
    python compact2fst.py  $i  > sources/$(basename $i ".formatoAmigo.txt").txt
 done
 
 
 # ############ convert words to openfst ############
 for w in tests/*.str; do
-	#echo "Converting words: $w"
 	python word2fst.py `cat $w` > tests/$(basename $w ".str").txt
 done
 
 
 # ############ Compile source transducers ############
 for i in sources/*.txt tests/*.txt; do
-	#echo "Compiling: $i"
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 done
-
-# ############ CORE OF THE PROJECT  ############
-
-
-
-
-
-
 
 <<com
 # ############ generate PDFs  ############
@@ -106,19 +95,24 @@ for w in compiled/t-step9*.fst; do
 done
 
 fstcompose compiled/step1.fst compiled/step2.fst > compiled/meta12.fst
-fstcompose compiled/step12.fst compiled/step3.fst > compiled/meta123.fst
-fstcompose compiled/step123.fst compiled/step4.fst > compiled/meta1234.fst
-fstcompose compiled/step1234.fst compiled/step5.fst > compiled/meta12345.fst
-fstcompose compiled/step12345.fst compiled/step6.fst > compiled/meta123456.fst
-fstcompose compiled/step123456.fst compiled/step7.fst > compiled/meta1234567.fst
-fstcompose compiled/step1234567.fst compiled/step8.fst > compiled/meta12345678.fst
-fstcompose compiled/step12345678.fst compiled/step9.fst > compiled/metaphoneLN.fst
+fstcompose compiled/meta12.fst compiled/step3.fst > compiled/meta123.fst
+fstcompose compiled/meta123.fst compiled/step4.fst > compiled/meta1234.fst
+fstcompose compiled/meta1234.fst compiled/step5.fst > compiled/meta12345.fst
+fstcompose compiled/meta12345.fst compiled/step6.fst > compiled/meta123456.fst
+fstcompose compiled/meta123456.fst compiled/step7.fst > compiled/meta1234567.fst
+fstcompose compiled/meta1234567.fst compiled/step8.fst > compiled/meta12345678.fst
+fstcompose compiled/meta12345678.fst compiled/step9.fst > compiled/metaphoneLN.fst
 
-#rm compiled/meta1*
+
+rm compiled/meta1*
 
 echo "---------------------------------------- Testing MetaphoneLN ----------------------------------------"
 
 for w in compiled/t-*; do
+    echo $w
     fstcompose $w compiled/metaphoneLN.fst | fstshortestpath | fstproject --project_type=output |
     fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+    printf "\n"
 done
+
+fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt compiled/metaphoneLN.fst | dot -Tpdf > images/metaphoneLN.pdf
